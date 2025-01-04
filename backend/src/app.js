91dd5ab7964b78a745b4dc54774e8f32672648b0
sudiptadutta99 <sudiptadutta8445@gmail.com>
2025-01-04 07:24:25 +0530

    Ready for Deployment

---
+++ backend/src/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import {createServer} from 'node:http'; 
+
+import {Server} from 'socket.io';
+
+import mongoose from 'mongoose';
+import { connectToSocket } from './controllers/socketManager.js';
+
+import cors from 'cors';
+import userRoutes from './routes/users.routes.js';
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}));
+
+app.use("/api/v1/users", userRoutes);
+
+const start  = async () => {
+    app.set("mongo_user");
+    const connectionDb = await mongoose.connect('mongodb+srv://sudiptadutta8445:rGpNaMbyUlWHjoNW@cluster0.17jay.mongodb.net/');
+
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`); //mongodb+srv://sudiptadutta8445:rGpNaMbyUlWHjoNW@cluster0.17jay.mongodb.net/host`);
+    server.listen(app.get("port"), () => {
+        console.log('listening on port 8000');
+    });
+};
+
+start();
